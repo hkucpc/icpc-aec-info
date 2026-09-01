240231d7c127d87c298412ba0cbda02115e637d7
Yang Ming-Tian <i@skylee.xyz>
2026-09-01 13:46:03 +0800

    Remove sidebars (#17)

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -41,7 +41,7 @@ const config: Config = {
       {
         docs: {
           routeBasePath: '/',
-          sidebarPath: './sidebars.ts',
+          sidebarPath: false,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
